c1e491b43a50982262df4ab135dc98ce1b409fb9
ExtinctSpecie <skarralefter@hotmail.com>
2017-07-05 22:19:11 +0300

    project

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\workspace\desktop\a_projects\zantetravel\zantetravelapi\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
