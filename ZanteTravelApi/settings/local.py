f8eca9937e4bd696ea34dce578c51b3584fd88f9
ExtinctSpecie <skarralefter@hotmail.com>
2017-07-18 17:01:49 +0300

    added css

--- ZanteTravelApi/settings/local.py
+++ ZanteTravelApi/settings/local.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 SECRET_KEY = 'd*f8o5_4%8u*v2a$vriw=jtb1pasr&kv-e*jlq-88prj8_i5x!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['zante-travel.herokuapp.com','localhost']
 
@@ -127,8 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
