45bd3684e4280c3a08dbe3138d701527a9b09978
ExtinctSpecie <skarralefter@hotmail.com>
2017-07-18 17:32:16 +0300

    added css

--- ZanteTravelApi/urls.py
+++ ZanteTravelApi/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf import settings
-from django.conf.urls import url, include
+from django.conf.urls import include , url
 from django.conf.urls.static import static
 from django.contrib import admin
 from rest_framework.urlpatterns import format_suffix_patterns
@@ -23,6 +23,6 @@ urlpatterns = [
     url(r'^lefterisAdministrator/', admin.site.urls),
     url(r'^api/', include('api.urls')),
 ]
-urlpatterns = format_suffix_patterns(urlpatterns)
+#urlpatterns = format_suffix_patterns(urlpatterns)
 urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
